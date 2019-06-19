d7a1ce58a46800020727c6d42a184c804ea56676
David H. Silber <david.silber@cognizant.com>
2019-06-19 10:56:19 -0400

    Added .NET sample work

---
+++ ASP_MVC/Controllers/WorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASP_MVC.Controllers
+{
+    public class WorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name = "Guest")
+        {
+            ViewData["Message"] = $"I'm so excited to meet you {name}!";
+            return View();
+        }
+    }
+}
